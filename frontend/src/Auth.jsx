2f1500f31d5ad2c885df5b024691143540d0898c
FouedBenRhayem <fouedrhaiem882@gmail.com>
2026-08-07 22:28:48 +0100

    Fix duplicate API_URL in Auth.jsx

--- frontend/src/Auth.jsx
+++ frontend/src/Auth.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const API_URL = process.env.REACT_APP_API_URL || "";
 
 
-const API_URL = process.env.REACT_APP_API_URL || "";
+
 
 const VIDEO_SRC =
   "https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260315_073750_51473149-4350-4920-ae24-c8214286f323.mp4";
